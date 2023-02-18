70bfdcf692acaec241eb917bafcf3e445bcfb091
meiliou <83768277+meiliou@users.noreply.github.com>
2023-02-18 18:19:27 +0200

    attempting to add loop to window alerts

--- Develop/script.js
+++ Develop/script.js
@@ -7,9 +7,28 @@ var lowerChars = "abcdefghijklmnopqrstuvwxyz";
 var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbericChars = "0123456789";
 var specialChars = "!@#$%^&*()_+";
-var allChars = "";
+var chosenChars = "";
 var typesArr = "";
 var randomNumber = "";
+
+var passwordOptions = [ 
+  {
+    name: "lowercase letters", 
+    characters: "abcdefghijklmnopqrstuvwxyz"
+  },
+  {
+    name: "uppercase letters", 
+    characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  },
+  {
+    name: "numbers", 
+    characters: "0123456789"
+  },
+  {
+    name: "special characters", 
+    characters: "!@#$%^&*()_+"
+  },
+];
 
 
 // Get references to the #generate element
@@ -35,75 +54,24 @@ function generatePassword() {
     window.alert("Please pick a number between 8 and 128");
     generatePassword();
   }
+ 
+  var chooseOptions = function () {
+    // prompts to select character types
+    var choice = window.confirm("Would you like any " + passwordOptions.name[i] +  " in your password?");
+    if (choice) {
+      window.alert("adding " + passwordOptions.name[i]+ " to your password");
+      chosenChars = chosenChars + lowerChars;
+      console.log(chosenChars);
+    }
+    // aaaargh
+    for (var i = 0; i < passwordOptions.length; i++);
 
-
-
-
-// Prompts to confirm characters
-var lowerChoice = window.confirm("Would you like any lowercase letters in your password?");
-if (lowerChoice) {
-  alert("You chose to include lower case letters in your password.");
-  allChars += lowerChars;
-}
-else {
-  alert("You chose not to have any lower case letters in your password.");
-}
-
-var upperChoice = window.confirm("Would you like uppercase letters in your password?");
-if (upperChoice) {
-  alert("You chose to include upper case letters in your password.");
-  allChars = upperChars + allChars;
-}
-else {
-  alert("You chose not to have any upper case letters in your password.");
-}
-
-var numberChoice = window.confirm("Would you like to use numbers in your password?");
-if (numberChoice) {
-  alert("You chose to include numbers in your password. I mean, this could be good, or it could be bad.");
-  allChars += numberChars;
-}
-else {
-  alert("You chose not to have any numbers in your password. This is probably the right choice.");
-}
-
-var specialChoice = window.confirm("Would you like to use special characters (such as '!, ?, &, /, etc.' in your password?");
-if (specialChoice) {
-  alert("You chose to include special characters in your password.");
-  allChars += specialChars;
-}
-else {
-  alert("You chose not to have any special characters in your password.");
-}
-
-if (allChars) {
-  alert("Ok, let's spit out some stuff that looks like a password for you.");
-}
-else {
-  alert("Hey, bonehead. Pick at least one of the choices (lowercase letters, uppercase letters, numbers, and/or special characters) to include in your password! Otherwise, I have nothing to work with. Help me out here. Try again.");
-}
-
-var temp = new Array();
-  // randomized character to add to password
-  for (let i = 0; i < passwordLength; i++) {
-    randomNumber = Math.floor(Math.random() * allChars.length);
-    numbArray = allChars[randomNumber];
-    temp.push(numbArray);
+    var pickedOption = passwordOptions[i];
+    pickedOption
   }
+  chooseOptions();
 
-  // make code suitable for password
-  password = temp.join( " " );
-  return password; 
 }
-
-
-
-
-
-
-
-
-
 
 /*
 // --------- Confirm Criteria --------- //
@@ -111,32 +79,7 @@ var temp = new Array();
 var confirmCriteria = function() {
   // ask if they'd like to add criteria.type
   var promptCriteria = window.prompt("Would you like to add " + criteria[i] + " ? Enter 'yes' or 'no' to choose.");
-  
-  // validate prompt answer
-  if (promptCriteria === "" || promptCriteria === null) {
-    window.alert("Please respond with 'yes' or 'no'.");
-    return confirmCriteria();
-  }    
 
-  // convert to lower case so less options to check
-  promptCriteria = promptCriteria.toLowerCase();  
-
-  // returns true if user says yes
-  if (promptCriteria === "yes") {
-    console.log("yep");
-    return true;
-  }
-  // returns false if user says no
-  if (promptCriteria === "no") {
-    console.log("nope");
-    return false;
-  }
-  // any other response will re-run this function 
-  else {
-    console.log("respond again");
-    return confirmCriteria();
-  }
-};
 
 // ----- Run and Remember Criteria choices ----- //
 var runCriteria = function () {
@@ -151,17 +94,6 @@ var runCriteria = function () {
 
 // --------- Generate Password --------- //
 */
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Add event listener to generate button
