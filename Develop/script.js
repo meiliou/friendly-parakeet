c9afe7b102143800f6b3b31f92aff7d483f11644
meiliou <83768277+meiliou@users.noreply.github.com>
2023-02-19 14:33:23 +0200

    moved event listeners to bottom

--- Develop/script.js
+++ Develop/script.js
@@ -29,21 +29,6 @@ var charInfo = [
   }
 ];
 
-// ********************************************* //
-// ---------- Pass-through functions ----------- //
-// ********************************************* //
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // ********************************************* //
 // ------------- Generate Password ------------- //
@@ -99,3 +84,18 @@ function generatePassword() {
   return password;
 };
 
+// ********************************************* //
+// ---------- Pass-through functions ----------- //
+// ********************************************* //
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
